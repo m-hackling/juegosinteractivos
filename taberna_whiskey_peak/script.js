dac66c74385aa36a4f3ece1b9cecf46f1fd48b2a
m-hackling <mhackling@hotmail.com>
2025-04-21 22:42:44 -0500

    mensajes e historia actualizada 1

--- taberna_whiskey_peak/script.js
+++ taberna_whiskey_peak/script.js
@@ -1,72 +1,359 @@
 let pelea = false;
 let reputacion = 0;
+let typingInterval;
+let isTyping = false;
+let typingIndicator;
+let currentText = "";
 
 const scenes = {
+    // Escena inicial
     Start: {
         character: "Bartender",
-        image: "https://i.ebayimg.com/images/g/dH4AAOSwC7VhyIww/s-l1200.png",
-        background: "",
-        text: `¡Bienvenido a la nueva taberna de Whiskey Peak!<br>Estamos celebrando nuestra reinauguración gracias a la ayuda de la tripulación Rakuen Kaizokudan.<br><br>Por favor, siéntete como en casa. ¿Deseas tomar algo o prefieres explorar el lugar?`,
+        image: "https://i.imgur.com/8LCUO8p.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `¡Bienvenido a la taberna de Whiskey Peak!<br>Estamos celebrando nuestra reinauguración con ayuda de Rakuen.<br><br>¡Te los voy a presentar para que los conozcas!`,
         options: [
-            { text: "Pedir algo de beber", next: "Sake" },
-            { text: "Explorar el lugar", next: "Explorar" }
+            { text: "Conocer a los integrantes", next: "PresentacionRakuen" },
         ]
     },
 
-    Sake: {
+    PresentacionRakuen: {
         character: "Bartender",
-        image: "https://i.ebayimg.com/images/g/dH4AAOSwC7VhyIww/s-l1200.png",
-        background: "https://i.imgur.com/SnAsQLs.png",
-        text: `Aquí tienes, una jarra de nuestro mejor sake. ¡Cortesía de la casa por la reinauguración!`,
+        image: "https://i.imgur.com/hIFLvwF.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `El es Aska, el capitán de la tripulación. Su iniciativa motivó a los demás para que adecuaran el interior y la fachada de la taberna.`,
         options: [
-            { text: "Agradecer y beber", next: "FinSake" },
-            { text: "Observar a los demás clientes", next: "FinSake" }
+            { text: "Hablar con Aska", next: "HablasAska" }
         ]
     },
 
-    Explorar: {
-        character: "Narrador",
-        image: "",
-        background: "https://i.imgur.com/lQT2bP4.png",
-        text: `La taberna ha sido remodelada con gusto. Hay decoraciones nuevas, banderas piratas colgadas y una tarima donde alguien parece estar afinando un violín.`,
+    HablasAska: {
+        character: "Aska",
+        image: "https://i.imgur.com/hIFLvwF.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `Muchas gracias por venir a la inauguración. Whiskey Peak debe ser un mejor lugar para todos.`,
         options: [
-            { text: "Acercarte a la tarima", next: "FinExplorar" },
-            { text: "Volver con la bartender", next: "Start" }
+            { text: "Conocer a la primer oficial", next: "PresentacionCamellia" }
         ]
     },
 
-    FinSake: {
+    PresentacionCamellia: {
+        character: "Bartender",
+        image: "https://i.imgur.com/p0qaRKM.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `Ella es Camellia, mano derecha del capitán. Su gran tamaño nos ayudó a reparar las instalaciones con más facilidad, sus arrugas denotan años de experiencia.`,
+        options: [
+            { text: "Hablar con Camellia", next: "HablasCamellia" }
+        ]
+    },
+
+    HablasCamellia: {
+        character: "Camellia",
+        image: "https://i.imgur.com/p0qaRKM.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `Hay mucho por hacer aquí, mientras tanto, espero encuentres un lugar cómodo por aquí.`,
+        options: [
+            { text: "Conocer al navegante", next: "PresentacionRelmin" }
+        ]
+    },
+
+    PresentacionRelmin: {
+        character: "Bartender",
+        image: "https://i.imgur.com/D6av3eI.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `Este niño se llama Relmin, desde que ha venido aquí no le ha quitado los ojos de encima a las camareras...`,
+        options: [
+            { text: "Hablar con Relmin", next: "HablasRelmin" }
+        ]
+    },
+
+    HablasRelmin: {
+        character: "Relmin",
+        image: "https://i.imgur.com/D6av3eI.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `¡AAAAAAAAAAAAAAAAAAAAAAAAH! ¡Venir aquí ha sido lo mejor que me ha pasado! ♡u♡`,
+        options: [
+            { text: "Conocer al carpintero", next: "PresentacionBamby" }
+        ]
+    },
+    ////////VAS POR AQUI
+    PresentacionBamby: {
+        character: "Bartender",
+        image: "https://i.imgur.com/D6av3eI.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `¡AAAAAAAAAAAAAAAAAAAAAAAAH! ¡Venir aquí ha sido lo mejor que me ha pasado! ♡u♡`,
+        options: [
+            { text: "Conocer al carpintero", next: "PresentacionBamby" }
+        ]
+    },
+
+    // Ruta 1 y 4
+    TomarSake1: {
+        character: "Bartender",
+        image: "https://i.imgur.com/8LCUO8p.png",
+        background: "https://i.imgur.com/0WVQzdJ.png",
+        soundEffect: "",
+        text: `Aquí tienes, nuestro mejor sake. ¡Disfrútalo!`,
+        options: [
+            { text: "Beber y girarte a mirar el lugar", next: "GirarYObservar" },
+            { text: "Beber otro sin girarte", next: "TomarSake2" }
+        ]
+    },
+
+    // Ramas de tomar sake sin girarse
+    TomarSake2: {
         character: "Narrador",
         image: "",
-        background: "",
-        text: `El sake está delicioso. La atmósfera se llena de risas y música. Parece que esta noche será inolvidable...<br><br><em>FIN (por ahora)</em>`,
+        background: "https://i.imgur.com/0WVQzdJ.png",
+        soundEffect: "",
+        text: `Decides seguir bebiendo. El ambiente es festivo, pero algo turbio...`,
+        options: [
+            { text: "Beber otro más", next: "TomarSake3" },
+            { text: "Salir de la taberna", next: "FinalNeutral1" }
+        ]
+    },
+
+    TomarSake3: {
+        character: "Narrador",
+        image: "",
+        background: "https://i.imgur.com/0WVQzdJ.png",
+        soundEffect: "",
+        text: `Ya estás algo ebrio. El bartender se acerca preocupado.`,
+        options: [
+            { text: "Beber otro", next: "FinalMalo1" }
+        ]
+    },
+
+    FinalNeutral1: {
+        character: "Narrador",
+        image: "",
+        background: "https://i.imgur.com/0WVQzdJ.png",
+        soundEffect: "",
+        text: `Sales tambaleante de la taberna. Tal vez mañana recuerdes algo de esto.<br><br><em>FIN (neutral)</em>`,
         options: [
             { text: "Volver al inicio", next: "Start" }
         ]
     },
 
-    FinExplorar: {
+    FinalMalo1: {
+        character: "Narrador",
+        image: "",
+        background: "https://i.imgur.com/0WVQzdJ.png",
+        soundEffect: "",
+        text: `En tu borrachera, empujas al bartender sin querer. Los guardias te sacan a empujones.<br><br><em>FIN (malo)</em>`,
+        options: [
+            { text: "Volver al inicio", next: "Start" }
+        ]
+    },
+
+    GirarYObservar: {
+        character: "Narrador",
+        image: "https://i.imgur.com/LOqewoO.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `Notas al bartender preocupado. Algo no está bien.`,
+        options: [
+            { text: "Preguntar si necesita ayuda", next: "OfrecerAyuda" }
+        ]
+    },
+
+    // Ofreces ayuda
+    OfrecerAyuda: {
+        character: "Bartender",
+        image: "https://i.imgur.com/8LCUO8p.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `Sí... me vendría bien ayuda para terminar de organizar la taberna.`,
+        options: [
+            { text: "Ayudarle con la taberna", next: "AyudarOrganizar" },
+            { text: "No ayudar", next: "NoAyudar" }
+        ]
+    },
+
+    NoAyudar: {
         character: "Narrador",
         image: "",
         background: "",
-        text: `Te acercas a la tarima y notas que el músico es un mink. Al verte, asiente con una sonrisa, y comienza a tocar una melodía tranquila que inunda todo el lugar.<br><br><em>FIN (por ahora)</em>`,
+        soundEffect: "",
+        text: `El bartender suspira. Aun así, sigue limpiando solo. Lo notas muy tenso.`,
+        options: [
+            { text: "Preguntar qué le preocupa", next: "HistoriaExtorsion" },
+            { text: "Ignorar y marcharte", next: "FinalNeutral3" }
+        ]
+    },
+
+    FinalNeutral3: {
+        character: "Narrador",
+        image: "",
+        background: "",
+        soundEffect: "",
+        text: `Decides no involucrarte más. Abandonas la taberna.<br><br><em>FIN (neutral)</em>`,
+        options: [
+            { text: "Volver al inicio", next: "Start" }
+        ]
+    },
+
+    AyudarOrganizar: {
+        character: "Narrador",
+        image: "",
+        background: "",
+        soundEffect: "",
+        text: `Ayudas a limpiar y ordenar. El lugar empieza a lucir mejor.`,
+        options: [
+            { text: "Seguir organizando", next: "OrganizarMas" },
+            { text: "Explorar un cuarto misterioso", next: "InvestigarCuarto" }
+        ]
+    },
+
+    OrganizarMas: {
+        character: "Narrador",
+        image: "https://i.imgur.com/LOqewoO.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "",
+        text: `Terminas de organizar. Pero el bartender sigue preocupado.`,
+        options: [
+            { text: "Preguntar qué le preocupa", next: "HistoriaExtorsion" }
+        ]
+    },
+
+    InvestigarCuarto: {
+        character: "Narrador",
+        image: "https://i.imgur.com/MRdT26V.png",
+        background: "https://i.imgur.com/6I14sev.png",
+        soundEffect: "grab.mp3",
+        text: `Encuentras un par de guantes de acero escondidos entre cajas. Podrían ser útiles.`,
+        options: [
+            { text: "Volver y hablar con el bartender", next: "HistoriaExtorsionConGuantes" }
+        ]
+    },
+
+    HistoriaExtorsion: {
+        character: "Bartender",
+        image: "https://i.imgur.com/LOqewoO.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "cry.mp3",
+        text: `Hace unos días, unos piratas comenzaron a pedirme "protección". Si no pago, arrasarán la taberna.`,
+        options: [
+            { text: "Ayudarle con los piratas", next: "PeleaSinGuantes" },
+            { text: "No ayudarle", next: "NoAyudarConPiratas" }
+        ]
+    },
+
+    HistoriaExtorsionConGuantes: {
+        character: "Bartender",
+        image: "https://i.imgur.com/LOqewoO.png",
+        background: "https://i.imgur.com/95tv4Uv.png",
+        soundEffect: "cry.mp3",
+        text: `Hace unos días, unos piratas comenzaron a pedirme "protección". Si no pago, arrasarán la taberna.`,
+        options: [
+            { text: "Ayudarle con los piratas", next: "PeleaConGuantes" },
+            { text: "No ayudarle", next: "NoAyudarConPiratas" }
+        ]
+    },
+
+    NoAyudarConPiratas: {
+        character: "Narrador",
+        image: "",
+        background: "",
+        soundEffect: "",
+        text: `El bartender asiente resignado. Se da vuelta y sigue limpiando solo.`,
+        options: [
+            { text: "Robarle mientras está distraído", next: "FinalMalo2" },
+            { text: "Marcharte", next: "FinalNeutral3" }
+        ]
+    },
+
+    FinalMalo2: {
+        character: "Narrador",
+        image: "",
+        background: "",
+        soundEffect: "",
+        text: `Aprovechas el descuido del bartender y te llevas algunas monedas. No te sientes orgulloso...<br><br><em>FIN (malo)</em>`,
+        options: [
+            { text: "Volver al inicio", next: "Start" }
+        ]
+    },
+
+    PeleaConGuantes: {
+        character: "Narrador",
+        image: "",
+        background: "",
+        soundEffect: "",
+        text: `Gracias a los guantes de acero, logras derrotar a los piratas tras una dura pelea. El bartender te abraza con gratitud.<br><br><em>¡FINAL BUENO! Toma un pantallazo y publícalo en el post del comunicado.</em>`,
+        options: [
+            { text: "Volver al inicio", next: "Start" }
+        ]
+    },
+
+    PeleaSinGuantes: {
+        character: "Narrador",
+        image: "",
+        background: "",
+        soundEffect: "",
+        text: `Intentas luchar contra los piratas, pero sin un arma adecuada te superan fácilmente. Te despiertas fuera de la taberna con un chichón...<br><br><em>FIN (malo)</em>`,
         options: [
             { text: "Volver al inicio", next: "Start" }
         ]
     }
 };
 
+function typeText(element, text, index = 0) {
+    isTyping = true;
+    currentText = text;
+
+    typingIndicator.classList.add("visible");
+
+    if (index < text.length) {
+        let char = text[index];
+
+        // Detectar etiquetas HTML
+        if (char === "<") {
+            const closeIndex = text.indexOf(">", index);
+            if (closeIndex !== -1) {
+                element.innerHTML += text.slice(index, closeIndex + 1);
+                typingInterval = setTimeout(() => typeText(element, text, closeIndex + 1), 10);
+                return;
+            }
+        }
+
+        element.innerHTML += char;
+        typingInterval = setTimeout(() => typeText(element, text, index + 1), 20);
+    } else {
+        isTyping = false;
+        typingIndicator.classList.remove("visible");
+    }
+}
+
 function showScene(key) {
     const content = scenes[key];
     if (!content) return;
 
     document.getElementById("character-name").textContent = content.character || "";
-    document.getElementById("dialogue-text").innerHTML = content.text || "";
     document.getElementById("character-image").src = content.image || "";
     document.getElementById("character-box").style.backgroundImage = "url('" + content.background + "')";
     
+    // Detiene cualquier animación anterior
+    clearTimeout(typingInterval);
+    const dialogueText = document.getElementById("dialogue-text");
+    dialogueText.innerHTML = "";
+    typeText(dialogueText, content.text || "");
+
     const choices = document.getElementById("choices");
     choices.innerHTML = "";
+
+    if (content.soundEffect) {
+        const fx = new Audio(content.soundEffect);
+        fx.volume = 0.3;
+        fx.play();
+    }
 
     (content.options || []).forEach(opt => {
         const btn = document.createElement("button");
@@ -74,11 +361,12 @@ function showScene(key) {
         btn.onclick = () => showScene(opt.next);
         choices.appendChild(btn);
     });
+
+
 }
 
-showScene("Start");
-
 window.addEventListener("DOMContentLoaded", () => {
+    typingIndicator = document.getElementById("typing-indicator");
     const startBtn = document.getElementById("start-btn");
     const overlay = document.getElementById("overlay");
     const gameContainer = document.getElementById("game-container");
@@ -88,7 +376,7 @@ window.addEventListener("DOMContentLoaded", () => {
         overlay.style.display = "none";
         gameContainer.style.display = "block";
 
-        audio.volume = 0.1;
+        audio.volume = 0.3;
         audio.muted = false;
         audio.play().then(() => {
             console.log("Audio activado.");
@@ -97,6 +385,17 @@ window.addEventListener("DOMContentLoaded", () => {
         });
 
         showScene("Start");
+    });
+
+    // Permitir salto de animación
+    const dialogueBox = document.getElementById("dialogue-text");
+    dialogueBox.addEventListener("click", () => {
+        if (isTyping) {
+            clearTimeout(typingInterval);
+            dialogueBox.innerHTML = currentText;
+            typingIndicator.classList.remove("visible");
+            isTyping = false;
+        }
     });
 });
 
